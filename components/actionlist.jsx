54bbef06e643aa89154b5ec93348007cfd7efdfc
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-05 21:14:58 +0700

    update page

---
+++ components/actionlist.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function ActionList({ actions }) {
+  if (!actions || actions.length === 0) return null
+  return (
+    <div className="flex gap-2 max-w-96 px-2 py-1 overflow-x-auto flex-wrap items-center">
+      {actions.map((action, index) => (
+        <button
+          title={action}
+          className="border text-xs border-gray-500 bg-gray-400 px-2 py-0 rounded-lg text-center text-nowrap"
+          key={index}
+        >
+          {action}
+        </button>
+      ))}
+    </div>
+  )
+}
---
+++ icons/circleloading.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function CircleLoading(props) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      className="progress-ring indeterminate ring-progress-rotate"
+      viewBox="0 0 16 16"
+    >
+      <circle
+        cx="50%"
+        cy="50%"
+        r="7"
+        strokeDasharray="3"
+        strokeDashoffset="NaN"
+        className="ring-progress-rotate"
+      ></circle>
+    </svg>
+  )
+}
